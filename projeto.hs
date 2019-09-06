fe85c3ea58e0c46e82727282245b84b541b7b544
Guilherme Rodrigues Bueno <gbueno91@gmail.com>
2019-09-06 15:58:28 -0300

    Calculando distancia euclidiana entre pontos e formatando a saída

--- projeto.hs
+++ projeto.hs
@@ -12,33 +12,48 @@ pontosRotulados pts rotulos = filter (\pt -> (fst pt) `elem` (map fst rotulos)) 
 pontosNaoRotulados pts rotulos = filter (\pt -> (fst pt) `notElem` (map fst rotulos)) pts
 
 --2 - calcula a distancia euclidiana entre 2 pontos dados
-distanciaEuclidiana pt1 pt2 = sqrt (foldl (+) 0 $ [a*a | a <- (zipWith (-) pt1 pt2)])
+distanciaEuclidiana (label1, pt1) (label2, pt2) = (sqrt (foldl (+) 0 $ [a*a | a <- (zipWith (-) pt1 pt2)]), label1, label2)
 
 --inverte a posicao dos elementos das tuplas de uma lista [(a,b)] = [(b,a)]
 inverterTuplas l = (map (uncurry $ flip (,)) l)
 
+--converte as coordenadas dos pontos lidos do arquivo de String para Num
 converterParaNum (a,b) = (a, map fromInteger ([read c::Integer | c <-b]))
+
+--recebe a distancia calculada, a label do ponto que ja pertence a uma classe e a label do ponto que desejamos rotular
+rotulaPonto (dist, rotulado, rotular) rotulos = (rotular, snd (head (filter (\rot -> fst rot == rotulado) rotulos)))
+
+adicionaSaida rot rotulos = [(b, [a]:(fst rot)) | (a,b) <- rotulos, b == (snd rot)]
+
+formataSaida [] l = l
+formataSaida (a:as) l = if (snd a) `elem` map snd l
+ then formataSaida as ([if classe == (snd a) then (label ++ (fst a), classe) else (label, classe) | (label, classe) <- l])--(filter (\rot -> (fst rot) == (fst a)) l)
+ else formataSaida as (l++[a])
 
 processFile f =
  let allLines = lines f
+     --lendo o arquivo
      pontos = map (\line -> (head (words line), tail (words line))) (takeWhile (not . null) allLines)
-     rotulos = map (\line -> (head (words line), tail (words line))) (takeWhile (not . null) (reverse allLines))
+     rotulos = map (\line -> (head (words line), head (tail (words line)))) (takeWhile (not . null) (reverse allLines))
+
+     --separando os pontos lidos em rotulados e nao rotulados
      rotulados = pontosRotulados pontos rotulos
      naoRotulados = pontosNaoRotulados pontos rotulos
-     rotuladosInvertido = inverterTuplas (map converterParaNum rotulados) --convertendo os valores lidos do arquivo para o tipo Num para poder fazer as contas
-     naoRotuladosInvertido = inverterTuplas (map converterParaNum naoRotulados)
-     distancias = distanciaEuclidiana <$> (map fst rotuladosInvertido) <*> (map fst naoRotuladosInvertido)
-     --linhasNaoNulas = filter (\line -> null line == False) allLines
-     --pontos = map (\line -> (head (words line), tail (words line))) linhasNaoNulas
-    -- result = unlines pontos
- in distancias
+
+     --convertendo os pontos lidos de String para Num para poder calcular as distancias
+     naoRotuladosNum = (map converterParaNum naoRotulados)
+     rotuladosNum = (map converterParaNum rotulados)
+     
+     --calcula a distancia de todos os pontos rotulados para os que ainda nao estao rotulados e retorna uma lista de [(dist, labelRotulado, labelARotular)]
+     distancias = distanciaEuclidiana <$> rotuladosNum <*> naoRotuladosNum
+     distanciaMinima = minimum distancias
+     rotulos2 = (rotulaPonto distanciaMinima rotulos):rotulos
+
+     saida = inverterTuplas (formataSaida [([a],b) | (a,b) <- rotulos2] [])
+ in saida
 
 main = do
  file <- getContents
- let pontos = processFile file
- print pontos
--- [(a,b) | (a,b) <- pontos]
--- putStrLn unwords map fst pontos
--- writeFile "output.txt" [a | ([a],b) <- pontos]
--- writeFile "output.txt" [a | [a] <- pontos]
-
+ let saida = processFile file
+ --print (unlines [a ++ " " ++ unwords b | (a,b) <- saida])
+ writeFile "output.txt" (unlines [a ++ " " ++ unwords b | (a,b) <- saida])
